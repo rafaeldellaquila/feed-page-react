2af68f0a47992e58a0456e53eb555c1f2fdd73d7
rafaeldellaquila <rafael@dellaquila.dev>
2022-11-22 15:32:23 -0300

    :white_check_mark: add test to  postcard

--- src/components/PostCard/PostCard.spec.jsx
+++ src/components/PostCard/PostCard.spec.jsx
@@ -1,9 +1,23 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import PostCard from '.'
 import { postCardProps } from './mock'
 
 describe('<PostCard/>', () => {
   it('should render the PostCard', () => {
     render(<PostCard {...postCardProps} />)
+
+    expect(
+      screen.getByRole('img', { name: postCardProps.title })
+    ).toHaveAttribute('src', postCardProps.cover)
+
+    expect(
+      screen.getByRole('heading', { name: postCardProps.title })
+    ).toBeInTheDocument()
+
+    expect(screen.getByText(postCardProps.body)).toBeInTheDocument()
+  })
+  it('should match snapshot', () => {
+    const { container } = render(<PostCard {...postCardProps} />)
+    expect(container).toMatchSnapshot()
   })
 })
